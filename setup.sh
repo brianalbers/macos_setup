d8aaf765f88a9f648340d6c470b7d7219a0e851d
Brian Albers <balbers@Brians-MacBook-Pro-3.local>
2018-02-20 06:30:11 -0700

    dotfile location changes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
-#!/bin/zsh
-
-# Reference - https://github.com/mathiasbynens/dotfiles
+# https://github.com/mathiasbynens/dotfiles
 
 #Terminal
 # Set zsh as default shell
@@ -18,10 +16,10 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-cp -f .zpreztorc ~/.
+cp -f dotfiles/zpreztorc ~/.zpreztorc
 
 # Move gitconfig to home folder
-cp -f .gitconfig ~/.
+cp -f dotfiles/gitconfig ~/.gitconfig
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -45,7 +43,6 @@ brew install mas
 
 # Install Memory Clean 2
 # Retrieve ID by running "mas search 'Memory Clean 2'"
-echo "Installing Memory Clean 2 - Apple Store password required"
 mas signin brian@brianalbers.com
 mas install 1114591412
 
@@ -54,13 +51,14 @@ brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
 
+# Install other helpful utilities
 brew install nmap
 brew install ack
 brew install tree
 
-# Install AWS tools
-brew install s3cmd
-brew install awscli
+# Install iStat menu bar - se
+brew cask install istat-menus
+open /Applications/iStat\ Menus.app
 
 # Install Homebrew Cask
 brew tap caskroom/cask
@@ -70,15 +68,13 @@ brew install zsh-completions
 
 # Install iterm2
 brew cask install iterm2
+cp -f dotfiles/com.googlecode.iterm2.plist ~/Library/Preferences
 
 # Install Slack
 brew cask install slack
 
 # Install rocket.chat
 brew cask install rocket-chat
-
-# Install Webstorm
-brew cask install webstorm
 
 # Install Google Chrome
 brew cask install google-chrome
@@ -90,10 +86,12 @@ brew cask install spotify
 
 # Install VMware Fusion
 brew cask install caskroom/versions/vmware-fusion
+open /Applications/VMware\ Fusion.app
 #export serial=""
 #/Applications/VMware\ Fusion.app/Contents/Library/Initialize\ VMware\ Fusion.tool set "" "" ${serial}
 
-brew cask install evernote
+# Commented out due to license limitations of 2 computers
+#brew cask install evernote
 
 sudo ./citrix_and_cac.sh
 
@@ -105,4 +103,6 @@ brew cask cleanup
 
 ./macos.sh
 
-./setup.sh
+./dev_environments.sh
+
+./dock.sh
