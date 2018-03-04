e688906d505e4fabaf5fb0ceb169c53f2188db0a
Brian Albers <none@none.com>
2018-03-03 19:32:26 -0700

    script now waits until xcode dev tools have finished installing before continuing on.

--- setup.sh
+++ setup.sh
@@ -10,6 +10,16 @@
 
 # Install xcode dev tools for git
 xcode-select --install
+sleep 1
+check=$((xcode-\select --install) 2>&1)
+echo $check
+str="xcode-select: note: install requested for command line developer tools"
+while [[ "$check" == "$str" ]];
+do
+  sleep 1
+  check=$((xcode-\select --install) 2>&1)
+  echo -n "."
+done
 
 # Clone macos_setup project from github
 mkdir $HOME/dev
