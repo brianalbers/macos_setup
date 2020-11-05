da5c5304f9478162bd630d96bb934b4c49b29f96
Brian Albers <b.albers@fourseagroupinc.com>
2020-11-05 07:37:35 -0700

    updated dock with idea and a few other minor layout changes

--- scripts/dock.sh
+++ scripts/dock.sh
@@ -10,26 +10,22 @@ dockutil --remove all
 dockutil --add /System/Applications/System\ Preferences.app
 dockutil --add /System/Applications/Utilities/Keychain\ Access.app
 #dockutil --add /Applications/Utilities/Grab.app
-#dockutil --add '' --type spacer --section apps
+dockutil --add '' --type spacer --section apps
 dockutil --add /System/Applications/Reminders.app
 dockutil --add /System/Applications/Calendar.app
 dockutil --add /System/Applications/Notes.app
-dockutil --add /System/Applications/Music.app
 dockutil --add '' --type spacer --section apps
 dockutil --add /Applications/Safari.app
 dockutil --add /Applications/Google\ Chrome.app
 dockutil --add /Applications/Firefox\ Developer\ Edition.app
 dockutil --add /Applications/Slack.app
 dockutil --add /System/Applications/Messages.app
-dockutil --add /Applications/Signal.app
+dockutil --add /Applications/Microsoft\ Outlook.app
 dockutil --add '' --type spacer --section apps
+dockutil --add /Users/balbers/Applications/JetBrains\ Toolbox/IntelliJ\ IDEA\ Ultimate.app
 dockutil --add /Applications/iTerm.app
-dockutil --add /Applications/Sourcetree.app
 dockutil --add /Applications/Atom.app
-dockutil --add /Applications/WebStorm.app
-dockutil --add /Applications/PyCharm\ CE.app
-dockutil --add /Applications/Visual\ Studio\ Code.app
-dockutil --add /Applications/VMware\ Fusion.app
+dockutil --add /Applications/Sourcetree.app
 # dockutil --add '' --type spacer --section apps
 
 
